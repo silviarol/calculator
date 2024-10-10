6fb14a3f76c53d11de2f5ad9f4c786a195b2642a
silviarol <171265625+silviarol@users.noreply.github.com>
2024-10-10 17:53:31 +0200

    Create operation functions

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
+function add(numOne, numTwo) {
+    return numOne + numTwo;
+}
+
+function subtract(numOne, numTwo) {
+    return numOne - numTwo;
+}
+
+function multiply(numOne, numTwo) {
+    return numOne * numTwo;
+}
+
+function divide(numOne, numTwo) {
+    return numOne / numTwo;
+}
+
 const display = document.querySelector(".display");
 const one = document.querySelector(".one");
 const two = document.querySelector(".two");
-const three = document.querySelector(".three");
-const four = document.querySelector(".four");
-const five = document.querySelector(".five");
-const six = document.querySelector(".six");
-const seven = document.querySelector(".seven");
-const eight = document.querySelector(".eight");
-const nine = document.querySelector(".nine");
-const zero = document.querySelector(".zero");
-const plus = document.querySelector(".plus");
-const minus = document.querySelector(".minus");
-const divide = document.querySelector(".divide");
-const multiply = document.querySelector(".multiply");
-const equals = document.querySelector(".equals");
-const clear = document.querySelector(".clear");
 
+function operate(a, b, fn){
+    return fn(a, b);
+}
 
+let displayValue = display.innerHTML;
 
-
-
-
+let result = operate(1, 2, add);
+display.innerHTML = result;
