1876b4fab76ff019678b0944314fc37c6b3280f8
silviarol <171265625+silviarol@users.noreply.github.com>
2024-10-18 19:02:57 +0200

    Fix errors displayed

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ let n2 = "";
 let operands = ["", ""];
 let operators = [""];
 let result = "";
-let array = [];
-const displayMaxLength = 7;
+let arraySecondNumber = [];
+const displayMaxLength = 8;
 
 const display = document.querySelector(".display");
 
@@ -16,17 +16,21 @@ document.querySelectorAll(".number, .point").forEach((button) => button.addEvent
     let points = displayed.filter((element) => element == ".");
     
     if (n1 != "") {   
+        // Resets display when n2 is inputted
+        // Allows n2 to be bigger than 1 digit
         display.textContent = "";
         display.textContent += event.target.innerHTML;
-        array.push(display.textContent);
-        let secondNumber = array.join("").replace(/\.+/g, '.'); 
+        arraySecondNumber.push(display.textContent);
+        let secondNumber = arraySecondNumber.join("").replace(/\.+/g, '.'); 
         display.textContent = secondNumber;
     } else if (points.length > 1) {
         display.textContent = display.textContent.replace(/\.+/g, '.'); 
     }
 
     if (display.textContent.length > displayMaxLength) {
-    display.textContent = "ERROR!!";
+        display.textContent = "OVERFLOW";
+    } else if (display.textContent == "NO / BY 0" || display.textContent == "ERROR 404") {
+
     }
 })); 
 
@@ -44,15 +48,18 @@ document.querySelectorAll(".operator").forEach((button) => button.addEventListen
         n2 = display.textContent;
         operands[1] = n2;
         result = operate(n1, n2, operator); 
-        display.textContent = Math.round(result * 100) / 100; 
-        n1 = result;
-        operands[0] = n1;
-        clearOperatorAndn2()
-        operators[0] = event.target.innerHTML;
-        operator = operators[0];
-    }
-    
-    console.log(operands)
+
+        if (result == "NO / BY 0" || result == "ERROR 404") {
+            display.textContent = result;
+        } else {
+            display.textContent = Math.round(result * 100) / 100; 
+            n1 = display.textContent;
+            operands[0] = n1;
+            clearOperatorAndn2()
+            operators[0] = event.target.innerHTML;
+            operator = operators[0];
+        }
+    } 
 })); 
 
 function operate(n1, n2, operator) {
@@ -68,29 +75,31 @@ function operate(n1, n2, operator) {
         case "×":
             return a * b;
         case "÷":
-            return b === 0 ? "ERROR!!" : a / b;
+            return b === 0 ? "NO / BY 0" : a / b;
         default:
-            return "ERROR!!";
+            return "ERROR 404";
     }
 }
 
 document.querySelector(".equals").addEventListener("click", () => {
     if (n1 == "" && operator == "") {
-        display.textContent = "ERROR!!";
+        display.textContent = "ERROR 404";
     } else if (n1 == "" && operator != "") {
-        display.textContent = "ERROR!!";
+        display.textContent = "ERROR 404";
     } else if (n1 != "" && operator != "") {
         n2 = display.textContent;
         operands[1] = n2;
         result = operate(n1, n2, operator);
-        display.textContent = Math.round(result * 100) / 100;
-        n1 = result;
-        operands[0] = "";
-        clearOperatorAndn2()
-    } else if (n2 == "") {
-        display.textContent = "ERROR!!";
-    }
 
+        if (result == "NO / BY 0" || result == "ERROR 404") {
+            display.textContent = result;
+        } else {
+            display.textContent = Math.round(result * 100) / 100; 
+            n1 = display.textContent;
+            operands[0] = n1;
+            clearOperatorAndn2();
+        }
+    }  
 });
 
 document.querySelector(".clear").addEventListener("click", () => {
@@ -109,11 +118,10 @@ function clearOperatorAndn2() {
     operator = "";
     operands[1] = "";
     operators[0] = "";
-    array = [];
+    arraySecondNumber = [];
 }
 
 document.querySelector(".delete").addEventListener('click', () => {
-
 
     if (operator == "") {
         const currentDisplay = display.textContent.slice(0, -1);
@@ -129,6 +137,7 @@ document.querySelector(".delete").addEventListener('click', () => {
         }
 
         operands[0] = n1;
+
     } else {
         const currentDisplay = display.textContent.slice(0, -1);
         display.textContent = currentDisplay;
@@ -147,5 +156,12 @@ document.querySelector(".delete").addEventListener('click', () => {
 });
 
 document.querySelector(".negative").addEventListener('click', () => {
-    display.textContent *= -1;
+
+    if (display.textContent == "NO / BY 0" || display.textContent == "ERROR 404" 
+        || display.textContent == "OVERFLOW") {
+        display.textContent == "ERROR 404"
+    } else {
+        display.textContent *= -1;
+    }
+
 });
