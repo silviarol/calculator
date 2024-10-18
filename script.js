1aef3233cb97d8c1ed5a5e798fb168936b90d1fe
silviarol <171265625+silviarol@users.noreply.github.com>
2024-10-18 15:56:28 +0200

    Fix equals button logic

--- script.js
+++ script.js
@@ -3,57 +3,58 @@ let n1 = "";
 let n2 = "";
 let operands = ["", ""];
 let operators = [""];
-operands.length = 2;
-operators.length = 1;
-const fixedArray = operands.length;
-const fixedArrayTwo = operators.length;
 let result = "";
 let array = [];
 const displayMaxLength = 7;
 
-const btn = document.querySelectorAll(".number, .operator, .point");
-const decimal = document.querySelector(".point");
 const negative = document.querySelector(".negative");
-const clear = document.querySelector(".clear");
-const deleter = document.querySelector(".delete");
 const display = document.querySelector(".display");
-const equals = document.querySelector(".equals");
-const numberBtn = document.querySelectorAll(".number");
-const operatorBtn = document.querySelectorAll(".operator");
 
-numberBtn.forEach((button) => button.addEventListener("click", (event) => {    
+document.querySelectorAll(".number, .point").forEach((button) => button.addEventListener("click", (event) => {    
     
     display.textContent += event.target.innerHTML;
+    let displayed = display.textContent.split("");
+    let points = displayed.filter((element) => element == ".");
     
-    if (operator != "") {   
+    if (n1 != "") {   
         display.textContent = "";
         display.textContent += event.target.innerHTML;
         array.push(display.textContent);
-        let secondNumber = array.join("");
+        let secondNumber = array.join("").replace(/\.+/g, '.'); 
         display.textContent = secondNumber;
+    } else if (points.length > 1) {
+        display.textContent = display.textContent.replace(/\.+/g, '.'); 
     }
 
+    if (display.textContent.length > displayMaxLength) {
+    display.textContent = "ERROR!!";
+    }
 })); 
 
-operatorBtn.forEach((button) => button.addEventListener("click", (event) => {
-        
-    if (n1 == "" && operator == "") {
+document.querySelectorAll(".operator").forEach((button) => button.addEventListener("click", (event) => {
+      
+    if (n1 == "") {
         n1 = display.textContent;
-        operator = event.target.innerHTML;
         operands[0] = n1;
-        operators[0] = operator;
-    } else if (n1 != "" && operator != "") {   
-        n2 = display.textContent;
-        operands[1] = n2;
-        operators[0] = "";
         operator = event.target.innerHTML;
         operators[0] = operator;
-    } else if (n1 == result) {   
-        n2 = display.textContent;
-        operands[1] = n2;
-        operators[0] = "";
+    } else if (n1 != "" && operator =="") {   
         operator = event.target.innerHTML;
         operators[0] = operator;
+    } else if (n1 != "" && operator !="") {   
+        n2 = display.textContent;
+        operands[1] = n2;
+        result = operate(n1, n2, operator); 
+        display.innerHTML = Math.round(result * 100) / 100; 
+        n1 = result;
+        operands[0] = n1;
+        operators[0] = "";
+        operator = operators[1];
+        operators[0] = event.target.innerHTML;
+        operator = operators[0];
+        operands[1] = "";
+        n2 = "" ;
+        array = [];
     }
 
 })); 
@@ -77,34 +78,31 @@ function operate(n1, n2, operator) {
     }
 }
 
-equals.addEventListener("click", () => {
+document.querySelector(".equals").addEventListener("click", () => {
     if (n1 == "" && operator == "") {
         display.innerHTML = "ERROR!!";
-    } else if (n1 != "" && operator == "") {
+    } else if (n1 == "" && operator != "") {
         display.innerHTML = "ERROR!!";
-    } else {
+    } else if (n1 != "" && operator != "") {
         n2 = display.textContent;
+        operands[1] = n2;
         result = operate(n1, n2, operator);
-        display.innerHTML = result;
         display.innerHTML = Math.round(result * 100) / 100;
         n1 = result;
-        operands[0] = n1;
-        operators[0] = "";
-        array = [];
+        operands[0] = "";
         operands[1] = "";
+        n2 = "";
+        operator = "";
+        array = [];
     }
 
 });
 
-decimal.addEventListener("click", () => {
-    display.textContent += ".";
+document.querySelector(".clear").addEventListener("click", () => {
+    clearMemory();
 });
 
-clear.addEventListener("click", () => {
-    clearDisplay();
-});
-
-function clearDisplay() {
+function clearMemory() {
     display.innerText = "";
     n1 = "";
     n2 = "";
@@ -115,13 +113,16 @@ function clearDisplay() {
     array = [];
 }
 
-deleter.addEventListener('click', () => {
+document.querySelector(".delete").addEventListener('click', () => {
     const currentDisplay = display.innerHTML;
     result = operate(n1, n2, operator);
 
     if (result != "") {
         display.innerHTML = currentDisplay.slice(0, -1);
-        n1 = currentDisplay.slice(0, -1);  
+        n1 = currentDisplay.slice(0, -1);
+        if (n1 == "") {
+            clearMemory();
+        }  
         operands[0] = n1; 
     } else {
         display.innerHTML = currentDisplay.slice(0, -1);
